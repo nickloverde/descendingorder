f855eb3a4933e4d669e034d998c71d397a7fd46a
nickloverde <nick.loverde@gmail.com>
2021-03-16 10:26:51 -0500

    whiteboarded problem

---
+++ descending.js
@@ -0,0 +1,20 @@
+// Description:
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+// Whiteboarding
+// I know that I'll need to convert the number to an array first
+// But to create an array I need to convert the number to a string with toString
+// Then, I'll use .split to divide each character and put it in an array
+// Then, I'll use the .sort method which will put it in alphabetically or with numbers ascending order
+// Then, I'll reverse the ascending order
+// After that I'll join it again 
+// I can then use Number to put it back in a number or wrap everything in a parseInt and return that
+
+
